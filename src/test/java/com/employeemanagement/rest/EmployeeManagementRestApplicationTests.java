f128293aade87cafe1f98fc1aa743347e2ffd605
Mitadru Banik <37205320+mitadrub@users.noreply.github.com>
2021-05-22 11:56:36 +0530

    modified test

--- src/test/java/com/employeemanagement/rest/EmployeeManagementRestApplicationTests.java
+++ src/test/java/com/employeemanagement/rest/EmployeeManagementRestApplicationTests.java
@@ -1,5 +1,7 @@
 package com.employeemanagement.rest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class EmployeeManagementRestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
